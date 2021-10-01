61237cb4519cb025167bdcc4ef8f8a3fab660de7
Sofia Viarani <SOFIA.VIARANI@local.ittcesena.it>
2021-10-01 09:32:11 +0200

    V1.1: aggiunto ciclo per calcolo del minimo

--- minMAx/Program.cs
+++ minMAx/Program.cs
@@ -6,7 +6,19 @@ namespace minMAx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arrayVettore = new int[5] { 3, 6, -3, 5, 7 };
+            int min = arrayVettore[0];
+
+            for (int i = 1; i < 5; i++)
+            {
+                if (arrayVettore[i] < min)
+                {
+                    min = arrayVettore[i];
+                }
+            }
+
+            Console.WriteLine(min);
+            Console.ReadKey();
         }
     }
 }
